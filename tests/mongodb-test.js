9b368997ed99428ab97beccbb2a9599bf30613ab
Nataniel López <nataniel.lopez@fizzmod.com>
2019-05-31 22:56:59 -0300

    Unit test

--- tests/mongodb-test.js
+++ tests/mongodb-test.js
@@ -13,28 +13,85 @@ const { MongoDB, MongoDBError } = require('./../mongodb');
 
 describe('MongoDB', function() {
 
-	let dummyModel;
-	let fullTableName;
-
 	const mongodb = new MongoDB({});
 
+	// MongoDB.client class fake
+	mongodb.client = {
+		db: () => {
+			return {
+				collection: table => {
+					if(!table)
+						throw new Error();
+					else {
+						return {
+							createIndex: index => {
+								if(!index)
+									throw new Error();
+							},
+							find: filters => {
+								if(!filters)
+									throw new Error();
+								else {
+									return {
+										limit: limit => {
+											if(!limit)
+												throw new Error();
+											else {
+												return {
+													toArray: () => {}
+												};
+											}
+										}
+									};
+								}
+							}
+						};
+					}
+				}
+			};
+		}
+	};
+
 	class Model {
+
+		static get indexes() {
+			return [
+				'foo',
+				'bar'
+			];
+		}
+
 		getTable() {
 			return 'table';
 		}
 	}
 
-	before(() => {
-		dummyModel = new Model();
-		dummyModel.dbname = 'dbname';
-		fullTableName = `${dummyModel.dbname}.${dummyModel.getTable()}`;
-	});
-
 	describe('handled errors', function() {
 
 		describe('getFilter()', function() {
 
-			
+			it('should explode 😃🔥 with "model requires indexes"', function() {
+
+				assert.throws(() => {
+					mongodb.getFilter({});
+				}, {
+					name: 'MongoDBError',
+					code: MongoDBError.codes.MODEL_EMPTY_INDEXES
+				});
+			});
+
+			it('should explode 😃🔥 with "Operation requires indexes"', function() {
+
+				const model = new Model();
+
+				assert.throws(() => {
+					mongodb.getFilter(model);
+				}, {
+					name: 'MongoDBError',
+					code: MongoDBError.codes.EMPTY_INDEXES
+				});
+
+			});
 		});
 	});
 
@@ -48,7 +105,7 @@ describe('MongoDB', function() {
 
 		});
 
-		it('should not explode 🚫🔥', async function() {
+		it('should not explode 😱🔥', async function() {
 
 			const stub = stubExecute(
 				Promise.resolve()
@@ -58,6 +115,25 @@ describe('MongoDB', function() {
 
 			stub.restore();
 		});
+	});
+
+	describe('formatIndex()', function() {
+		it('should return formatted index object', function() {
+			assert.equal(typeof mongodb.formatIndex('index'), 'object');
+		});
+	});
+
+	describe('createIndexes()', function() {
+
+		// const checkConnectionStub = result => sinon.stub(MongoDB.prototype, 'checkConnection').callsFake(() => result);
+		// checkConnectionStub(true);
+
+		const model = new Model();
+
+		it('should not explode 😱🔥', async function() {
+			await assert.doesNotReject(mongodb.createIndexes(model));
+		});
+
 	});
 
 });
