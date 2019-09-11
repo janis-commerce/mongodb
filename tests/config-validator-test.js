ebc76b2253eaff85da9aa95cebb9948afa7bab8d
Nataniel López <nataniel.lopez@fizzmod.com>
2019-09-11 12:09:05 -0300

    Added more test cases

--- tests/config-validator-test.js
+++ tests/config-validator-test.js
@@ -10,7 +10,27 @@ describe('ConfigValidator', () => {
 
 	describe('validate()', () => {
 
+		it('should throw invalid config when the config is empty', () => {
+
+			assert.throws(() => ConfigValidator.validate(), {
+				name: 'MongoDBConfigError',
+				code: MongoDBConfigError.codes.INVALID_CONFIG
+			});
+
+		});
+
+
 		it('should throw invalid config when the config is not an object', () => {
+
+			assert.throws(() => ConfigValidator.validate('string'), {
+				name: 'MongoDBConfigError',
+				code: MongoDBConfigError.codes.INVALID_CONFIG
+			});
+
+		});
+
+
+		it('should throw invalid config when the config is an array', () => {
 
 			assert.throws(() => ConfigValidator.validate([]), {
 				name: 'MongoDBConfigError',
@@ -28,15 +48,28 @@ describe('ConfigValidator', () => {
 
 		});
 
-		it('should throw invalid setting when a setting has an unexpected type', () => {
 
-			assert.throws(() => ConfigValidator.validate({
-				database: 'myDB',
-				port: 'string'
-			}), {
-				name: 'MongoDBConfigError',
-				code: MongoDBConfigError.codes.INVALID_SETTING
+		['string', ['array']].forEach(type => {
+
+			it('should throw invalid setting when a setting has an unexpected type', () => {
+
+				assert.throws(() => ConfigValidator.validate({
+					database: 'myDB',
+					port: type
+				}), {
+					name: 'MongoDBConfigError',
+					code: MongoDBConfigError.codes.INVALID_SETTING
+				});
 			});
+
+		});
+
+		it('should not throw when the settings are correct', () => {
+
+			assert.doesNotThrow(() => ConfigValidator.validate({
+				database: 'myDB',
+				port: 27017
+			}));
 		});
 	});
 });
